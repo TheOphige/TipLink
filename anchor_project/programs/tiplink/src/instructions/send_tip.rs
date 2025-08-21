2123886d32b9f8dbdf5fc405ced3919db91581c4
TheOphige <igetheophilus03@gmail.com>
2025-08-21 05:07:22 +0100

    feat: derive the Tip PDA deterministically

--- anchor_project/programs/tiplink/src/instructions/send_tip.rs
+++ anchor_project/programs/tiplink/src/instructions/send_tip.rs
@@ -3,21 +3,29 @@ use crate::states::Tip;
 
 #[derive(Accounts)]
 pub struct SendTip<'info> {
-    #[account(init, payer = sender, space = 8 + 32 + 32 + 8 + 8)]
+    #[account(init, payer = sender, space = 8 + 32 + 32 + 8 + 32 + 8, seeds = [b"tip", sender.key().as_ref(), recipient.key().as_ref(), &Clock::get()?.unix_timestamp.to_le_bytes()], bump)]
     pub tip: Account<'info, Tip>,
+
     #[account(mut)]
     pub sender: Signer<'info>,
+
     /// CHECK: recipient can be any wallet
     pub recipient: UncheckedAccount<'info>,
+
     pub system_program: Program<'info, System>,
 }
 
 pub fn send_tip(ctx: Context<SendTip>, amount: u64, token_mint: Pubkey) -> Result<()> {
+    if amount == 0 {
+        return Err(error!(crate::errors::TipLinkError::InvalidAmount));
+    }
+
     let tip = &mut ctx.accounts.tip;
     tip.sender = *ctx.accounts.sender.key;
     tip.recipient = *ctx.accounts.recipient.key;
     tip.amount = amount;
     tip.token_mint = token_mint;
     tip.timestamp = Clock::get()?.unix_timestamp;
+
     Ok(())
 }
